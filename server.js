2ef2ea9eae9704f46fb58ab5dd52015d8ed94024
Dylan Shoemaker <me@dylanshoemaker.com>
2021-08-21 03:41:46 -0700

    provide err handling for 404

--- server.js
+++ server.js
@@ -42,6 +42,12 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+};
+
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -50,12 +56,17 @@ app.get('/api/animals', (req, res) => {
   res.json(results);
 });
 
-
-
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
 
-//added heroku, https://git.heroku.com/animal-crackr.git
-//https://animal-crackr.herokuapp.com/
+//added heroku, https://animal-crackr.herokuapp.com/api/animals
